43c0f03a6c320d721a7d0764622f858c2f8306c0
Niels Grewe <niels.grewe@halbordnung.de>
2017-02-21 09:37:25 +0100

    Tear apart the channel class into two interfaces.
    Channel in rxmq has two distinct responsibilities: It can be a basic pubsub message
    channel or it can be a request/response channel. It is impossible to get a safe interface
    for this in TypeScript, so we separate it into two interfaces, and have the user decide
    which one they want by using a generic bound on Rxmq.channel():
    
    * Channel<T> is the normal pubsub channel, which has a payload type for the data flowing over
      the channel.
    * RequestResponseChannel<Req,Res> is the request/response channel, with separate types for the
      payload of the request and the response (and a different payload structure for the Observable
      on the receiving side).

--- ts/channel.d.ts
+++ ts/channel.d.ts
@@ -5,18 +5,25 @@ export interface NewableSubject<U extends Subject<T>, T> {
     new(): U
 }
 
-export type RequestOptions<U extends Subject<T>, T> = {
-    topic: String,
-    data?: any,
-    Subject?: NewableSubject<U, T>
+export type RequestOptions<T, U extends Subject<R>, R> = {
+    topic: string,
+    data: T,
+    Subject?: NewableSubject<U, R>
 } 
 
-declare class Channel<T> {
-        constructor(plugins?: Array<Object>);
-        observe<T>(topic: String): Observable<T>
-        subject<T>(topic: String, subject?: EndlessSubject<T>): Subject<T>
-        request<T,U extends Subject<T>>(options: RequestOptions<U,T>): U
+declare interface BaseChannel {
+        new(plugins: Object[]): this
         registerPlugin(plugin: Object): void;
 }
 
-export default Channel;
+declare interface Channel<T> extends BaseChannel {
+        observe(topic: String): Observable<T>
+        subject(topic: String, subject?: EndlessSubject<T>): Subject<T>
+}
+
+declare interface RequestResponseChannel<Req,Res> extends BaseChannel {
+    observe(topic: String): Observable<{data: Req, replySubject: Subject<Res>}>
+    request<S extends Subject<Res>>(options: RequestOptions<Req, S, Res>): Subject<Res>
+}
+
+export  {Channel, RequestResponseChannel};
--- ts/rxmq.d.ts
+++ ts/rxmq.d.ts
@@ -1,14 +1,16 @@
-import Channel from './channel'
+import {Channel, RequestResponseChannel} from './channel'
 import EndlessSubject from './endlessSubject'
 import EndlessReplaySubject from './endlessReplaySubject'
 
+type ChannelType<T,R> = Channel<T> | RequestResponseChannel<T,R>
+
 declare namespace Rxmq {
-    function channel<T>(name: String): Channel<T>
+    function channel<T extends ChannelType<U,V>, U, V>(name: String): T
 
     function registerPlugin(plugin: Object): void
 
     function registerChannelPlugin(plugin: Object): void
 }
 
-export {Channel, EndlessSubject, EndlessReplaySubject};
+export {Channel, RequestResponseChannel, EndlessSubject, EndlessReplaySubject};
 export default Rxmq;
